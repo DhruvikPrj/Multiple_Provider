92eaa28cb2d00f90327e90242b05644f43dfee32
DhruvikPrj <dhruvik.prajapati@bypeopletechnologies.com>
2024-02-26 19:02:02 +0530

    Ended

--- lib/screens/Login_with_Provider/login.dart
+++ lib/screens/Login_with_Provider/login.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 //Loading Animations
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+
+//providers
 import 'package:multiple_provider/provider/login_auth_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -33,82 +35,90 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                label: Text("Enter Email"),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ValueListenableBuilder(
-              valueListenable: toggle,
-              builder: (context, value, child) {
-                return TextFormField(
-                  controller: passwordController,
-                  obscureText: toggle.value,
-                  decoration: InputDecoration(
-                      label: const Text("Enter Password"),
-                      suffix: GestureDetector(
-                          onTap: () {
-                            toggle.value = !toggle.value;
-                          },
-                          child: Icon(toggle.value
-                              ? Icons.visibility_off_rounded
-                              : Icons.visibility))),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            GestureDetector(
-              onTap: () {
-                authProvider.login(emailController.text.toString(),
-                    passwordController.text.toString());
-              },
-              child: Container(
-                  margin: const EdgeInsets.all(5),
-                  height: 50,
-                  //width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColorDark,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: List.filled(
-                        2, const BoxShadow(color: Colors.black87),
-                        growable: true),
+        child: SingleChildScrollView(
+          child: Builder(builder: (context) {
+            return SizedBox(
+              height: 800,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      label: Text("Enter Email"),
+                    ),
                   ),
-                  child: Center(
-                    child: authProvider.loading
-                        ? Container(
-                            padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                            height: 200,
-                            child: LoadingAnimationWidget.twistingDots(
-                              leftDotColor: const Color(0xFF1A1A3F),
-                              rightDotColor: Colors.white,
-                              size: 50,
-                            ),
-                          )
-                        :
-                        // const AlertDialog(
-                        //     title: Text('Login Failed'),
-                        //     content: Text('Please enter valid credentials...'),
-                        //   )
-                        const Text(
-                            "Login",
-                            style: TextStyle(color: Colors.white, fontSize: 20),
-                          ),
-                  )),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-          ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ValueListenableBuilder(
+                    valueListenable: toggle,
+                    builder: (context, value, child) {
+                      return TextFormField(
+                        controller: passwordController,
+                        obscureText: toggle.value,
+                        decoration: InputDecoration(
+                            label: const Text("Enter Password"),
+                            suffix: GestureDetector(
+                                onTap: () {
+                                  toggle.value = !toggle.value;
+                                },
+                                child: Icon(toggle.value
+                                    ? Icons.visibility_off_rounded
+                                    : Icons.visibility))),
+                      );
+                    },
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      authProvider.login(emailController.text.toString(),
+                          passwordController.text.toString());
+                    },
+                    child: Container(
+                        margin: const EdgeInsets.all(5),
+                        height: 50,
+                        //width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColorDark,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: List.filled(
+                              2, const BoxShadow(color: Colors.black87),
+                              growable: true),
+                        ),
+                        child: Center(
+                          child: authProvider.loading
+                              ? Container(
+                                  padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                                  height: 200,
+                                  child: LoadingAnimationWidget.twistingDots(
+                                    leftDotColor: const Color(0xFF1A1A3F),
+                                    rightDotColor: Colors.white,
+                                    size: 50,
+                                  ),
+                                )
+                              :
+                              // const AlertDialog(
+                              //     title: Text('Login Failed'),
+                              //     content: Text('Please enter valid credentials...'),
+                              //   )
+                              const Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 20),
+                                ),
+                        )),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            );
+          }),
         ),
       ),
     );
